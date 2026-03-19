0d99ce2514e71ec5229ab6d5a73866a6f36d2868
maycon_v_f <maycon.fedechen@escola.pr.gov.br>
2026-03-19 15:23:23 +0000

    .

---
+++ falso-v.js
@@ -0,0 +1,16 @@
+// booleano
+
+const usuarioLogado = true;
+const contaPaga = false;
+
+// truthy ou false;
+
+// 0 => false;
+// 1 => true;
+
+console.log (0 == false);
+console.log ("" == false);
+ console.log(1 == true);
+// null;
+// undefined;
+
