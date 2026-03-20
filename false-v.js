8b5623d37cf07a63ee87ec68cd32b0451e395f91
maycon_v_f <maycon.fedechen@escola.pr.gov.br>
2026-03-20 15:25:01 +0000

    .

---
+++ false-v.js
@@ -0,0 +1,32 @@
+// booleano
+
+//const usuarioLogado = true;
+//const contaPaga = false;
+
+// truthy ou false;
+
+// 0 => false;
+// 1 => true;
+
+//console.log (0 == false)
+//console.log ("" == false);
+ //console.log(1 == true);
+
+
+// undefined;
+
+// null ==> vasio ou nada;
+
+let minhaVar;
+let varNull = null;
+
+console.log (minhaVar);
+console.log (varNull);
+
+let numero = 3;
+let texto = "alura";
+
+console.log (typeof numero);
+console.log (typeof texto);
+
+
