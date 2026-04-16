192e882f08f145a2ab2606965535fb4d5099d07d
maycon_v_f <maycon.fedechen@escola.pr.gov.br>
2026-04-16 14:32:36 +0000

    .

---
+++ conversoes.js
@@ -0,0 +1,14 @@
+//tipo de dados 
+//booleanos
+
+// convesao implicita
+//const numero = 456;
+//const numeroString = Number ("456");
+
+//Number()
+//String()
+//console.log(numero + numeroString);
+
+//converção explicíta
+
+//calculadora explicitas
